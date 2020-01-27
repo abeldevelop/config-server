a15ce04e84698d11bdae0d76885ba0d1ac8e3d86
Abel Fernandez <abelfergodevelop@gmail.com>
2020-01-27 22:01:10 +0100

    Create the project

---
+++ src/main/java/com/abeldevelop/architecture/server/config/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.abeldevelop.architecture.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
